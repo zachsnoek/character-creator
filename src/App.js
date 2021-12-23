3cddbdec3cea627a649fc7d0d1c647e150b63dcf
ZachSnoek <zach.snoek@faithlife.com>
2021-12-23 15:34:20 -0500

    Complete exercise 4.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          height: "40%",
+          width: "100%",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
